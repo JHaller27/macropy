496d992a3f0401fc6079b2bdb40d5115c942c834
James <jameshaller27@gmail.com>
2021-08-27 22:38:42 -0700

    Bugfix

--- models.py
+++ models.py
@@ -1,9 +1,11 @@
-from typing import Literal, Union
+from typing import Literal, Union, Optional
 from pydantic import BaseModel
 from enum import Enum
 
 from time import sleep
 import keyboard
+
+from typer import secho, colors
 
 
 class EventType(str, Enum):
@@ -12,37 +14,42 @@ class EventType(str, Enum):
     LOOP = 'loop'
 
 
-class Event:
-    def execute(self) -> None:
-        raise NotImplementedError
-
-
-class DelayEvent(BaseModel, Event):
+class DelayEvent(BaseModel):
     type: Literal[EventType.DELAY]
     value: int
 
     def execute(self) -> None:
+        secho(f"Delay for {self.value} sec... ", nl=False, fg=colors.BRIGHT_BLACK)
         sleep(self.value)
+        secho("Done", fg=colors.BRIGHT_BLACK)
 
 
-class KeyEvent(BaseModel, Event):
+class KeyEvent(BaseModel):
     type: Literal[EventType.KEY]
     value: str
 
     def execute(self) -> None:
+        secho(f"Press key '{self.value}'... ", nl=False, fg=colors.BRIGHT_BLACK)
         keyboard.press_and_release(self.value)
+        secho("Done", fg=colors.BRIGHT_BLACK)
 
 
-class LoopEvent(BaseModel, Event):
+class LoopEvent(BaseModel):
     type: Literal[EventType.LOOP]
-    value: list[Event]
+    value: list[Union[DelayEvent, KeyEvent, 'LoopEvent']]
+    times: Optional[int]
 
     def execute(self) -> None:
-        for item in self.value:
-            item.execute()
+        secho(f"Looping {self.times} times... ", fg=colors.BRIGHT_BLACK)
+        i = self.times
+        while i is None or i > 0:
+            for item in self.value:
+                item.execute()
+            i -= 1
+        secho("End loop", fg=colors.BRIGHT_BLACK)
 
 
-class Config(BaseModel, Event):
+class Config(BaseModel):
     name: str
     sequence: list[Union[DelayEvent, KeyEvent, LoopEvent]]
 
